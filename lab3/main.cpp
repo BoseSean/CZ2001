4a5a36fefc89377dd5a572adb1136be3445ba864
BoseSean <xinyesg@gmail.com>
2017-10-21 18:29:26 +0800

    reduced test case size again

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -9,7 +9,8 @@
 #include "mergeSort.hpp"
 #include "insertionSort.hpp"
 
-#define ITERATION 20 
+#define ITERATION 10 
+
 #define GET_T1() t1 = std::chrono::high_resolution_clock::now()
 #define GET_T2() t2 = std::chrono::high_resolution_clock::now()
 static std::chrono::high_resolution_clock::time_point t1,t2;
@@ -25,33 +26,33 @@ static std::vector<int> generate_descending_vector(int size);
 
 int main() {
     std::cout<<std::setw(25)<<"merge"<<std::setw(15)<<"insert"<<std::setw(15)<<"merge"<<std::setw(15)<<"insert"<<std::endl;
-    std::cout<<std::setw(10)<<"Data size"<<std::setw(15)<<"Time use"<<std::setw(30)<<"Comparison"<<std::endl;
+    std::cout<<std::setw(10)<<"Data size"<<std::setw(15)<<"Time use(ms)"<<std::setw(30)<<"Comparison"<<std::endl;
     
     // 1000 to 10000 increase by 1000
-    for(int size=1000; size<10000; size+=1000){
-        std::vector<int> a = generate_random_vector(size,size);
-        merge_time = 0;
-        insert_time = 0;
-        for(int iter=0; iter<ITERATION; iter++){
-            std::vector<int> a = generate_random_vector(size,size);
-            std::vector<int> b = a; //make a copy of vector a
-            GET_T1();
-            MergeSort(a);
-            GET_T2();
-            merge_time+=std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
-            merge_compare += getMergeCompare();
+    // for(int size=1000; size<10000; size+=1000){
+    //     std::vector<int> a = generate_random_vector(size,size);
+    //     merge_time = 0;
+    //     insert_time = 0;
+    //     for(int iter=0; iter<ITERATION; iter++){
+    //         std::vector<int> a = generate_random_vector(size,size);
+    //         std::vector<int> b = a; //make a copy of vector a
+    //         GET_T1();
+    //         MergeSort(a);
+    //         GET_T2();
+    //         merge_time+=std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+    //         merge_compare += getMergeCompare();
 
-            GET_T1();
-            InsertionSort(b);
-            GET_T2();
-            insert_time+=std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
-            insert_compare += getInsertCompare();
-        }
+    //         GET_T1();
+    //         InsertionSort(b);
+    //         GET_T2();
+    //         insert_time+=std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+    //         insert_compare += getInsertCompare();
+    //     }
         
-        printValues(size, merge_time/ITERATION, insert_time/ITERATION, merge_compare/ITERATION, insert_compare/ITERATION);
-    }
+    //     printValues(size, merge_time/ITERATION, insert_time/ITERATION, merge_compare/ITERATION, insert_compare/ITERATION);
+    // }
     // 10000 to 1,000,000 increase by 10,000
-    for(int size=10000; size<1000000; size+=5000){
+    for(int size=10000; size<1000000; size+=20000){
         std::vector<int> a = generate_random_vector(size,size);
         merge_time = 0;
         insert_time = 0;
@@ -82,8 +83,8 @@ void printValues(int size, unsigned long long int merge_time, unsigned long long
     std::cout<<std::setw(10)<<size;
     std::cout<<std::setw(15)<<merge_time;
     std::cout<<std::setw(15)<<insert_time;
-    std::cout<<std::setw(15)<<merge_compare;
-    std::cout<<std::setw(15)<<insert_compare<<std::endl;
+    std::cout<<std::setw(20)<<merge_compare;
+    std::cout<<std::setw(20)<<insert_compare<<std::endl;
 }
 static std::vector<int> generate_random_vector(int size, int max){
  
